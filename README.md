f40307e7aeb01b46f085aef2c4a170b048e2bbc9
Michael Still <mikal@stillhq.com>
2019-03-23 14:36:16 +1100

    A simple status screen now works.

---
+++ README.md
@@ -0,0 +1,5 @@
+Credits
+=======
+
+* Material Design Icons are from https://cdn.materialdesignicons.com/3.5.95/.
+* BebaseNeue is licensed under the creative commons and is from https://fontlibrary.org/en/font/bebas
--- gangscan.py
+++ gangscan.py
@@ -13,6 +13,7 @@
 
 # A demo of LCD/TFT SCREEN DISPLAY
 
+import datetime
 import time
 import RPi.GPIO as GPIO
 
@@ -23,10 +24,15 @@ from PIL import ImageFont
 from lib_tft24T import TFT24T
 import spidev
 
-# For LCD TFT SCREEN:
+# For LCD TFT GPIOs
 DC = 24
 RST = 25
 LED = 23
+
+# Constants
+ICON_SIZE = 30
+
+print('%s Started' % datetime.datetime.now())
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -35,15 +41,41 @@ GPIO.setwarnings(False)
 # Initialize display.
 TFT = TFT24T(spidev.SpiDev(), GPIO, landscape=False)
 TFT.initLCD(DC, RST, LED)
+TFT.clear((175, 175, 175))
+print('%s Initialized screen' % datetime.datetime.now())
 
 # Objects we need to draw things
-draw = TFT.draw()
-font = ImageFont.truetype('FreeSerifItalic.ttf', 40)
-logo = Image.open('gangscan.jpeg')
-logo = logo.rotate(90, 0, 1).resize((240, 320))
+icons = ImageFont.truetype('materialdesignicons-webfont.ttf', ICON_SIZE)
+text = ImageFont.truetype('BebasNeue.ttf', ICON_SIZE)
 
-TFT.display(logo)
-draw.textwrapped((0,0), 'online: 192.168.1.1', 38, 20, font, 'lightblue')
-TFT.display()
+images = {}
+images['logo'] = Image.open('gangscan.jpeg').convert('RGBA')
+images['logo'] = images['logo'].rotate(90,
+                                       resample=0,
+                                       expand=1).resize((240, 320))
+print('%s Loaded logo' % datetime.datetime.now())
+
+for (icon_name, icon, font, inset) in [
+        ('wifi_on', unichr(0xf5a9), icons, 0),
+        ('wifi_off', unichr(0xf5aa), icons, 0),
+        ('connect_on', unichr(0xf1f5), icons, ICON_SIZE + 5),
+        ('connect_off', unichr(0xf1f8), icons, ICON_SIZE),
+        ('in', 'in', text, (ICON_SIZE + 5) * 2),
+        ('out', 'out', text, (ICON_SIZE + 5) * 2)]:
+    img = Image.new('RGBA', (320, 240))
+    img_writer = ImageDraw.Draw(img)
+    width, height = img_writer.textsize(icon, font=font)
+    img_writer.text(((ICON_SIZE - width) / 2 + 5, inset + 5),
+                    icon, fill='black', font=font)
+
+    images[icon_name] = img.rotate(90, resample=0, expand=1)
+    print(images[icon_name])
+    print('%s Loaded %s icon' %(datetime.datetime.now(), icon_name))
+
+status = images['logo']
+status = Image.alpha_composite(status, images['wifi_on'])
+status = Image.alpha_composite(status, images['connect_on'])
+status = Image.alpha_composite(status, images['out'])
+TFT.display(status)
 
 time.sleep(60)
