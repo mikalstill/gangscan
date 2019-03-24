07d714c027ed36f63031d0093cab4a5164f12af8
Michael Still <mikal@stillhq.com>
2019-03-24 16:38:46 +1100

    Writing and reading cards with hashes.

--- read_rfid.py
+++ read_rfid.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python
 
+import hashlib
+import json
 import sys
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522(bus=1, device=0, nrstpd=35)
+reader = SimpleMFRC522(bus=1, device=0, nrstpd=36)
+
+with open('config.json') as f:
+    config = json.loads(f.read())
 
 try:
-    cardid, text = reader.read()
-    print(cardid)
-    print(text)
+    while True:
+        cardid, text = reader.read()
+        text = text.rstrip(' ')
+        owner = text[:-6]
+        sig = text[-6:]
+
+        h = hashlib.sha256()
+        h.update(owner)
+        h.update(str(cardid))
+        h.update(config['pre-shared-key'])
+        s = h.hexdigest()[-6:]
+
+        outcome = True
+        if s != sig:
+            outcome = False
+
+        print json.dumps({'cardid': cardid,
+                          'owner': owner,
+                          'sha': sig,
+                          'outcome': outcome})
 
 finally:
     GPIO.cleanup()
--- write_rfid.py
+++ write_rfid.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python
 
+import hashlib
+import json
+
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522()
+reader = SimpleMFRC522(bus=1, device=0, nrstpd=3)
+
+with open('config.json') as f:
+    config = json.loads(f.read())
 
 try:
-    reader.write('This is a card!')
+    owner = raw_input('Card owner: ')
+    owner = owner[:64 - 7]
+    print('Stored owner is %s' % owner)
+
+    print('Place card on reader...')
+    cardid, text = reader.read()
+    print('Card id is %s' % cardid)
+
+    h = hashlib.sha256()
+    h.update(owner)
+    h.update(str(cardid))
+    h.update(config['pre-shared-key'])
+    print('Full hash is %s' % h.hexdigest())
+    s = h.hexdigest()[-6:]
+    print('Short hash is %s' % s)
+    
+    reader.write('%s%s' %(owner, s))
     print 'Written'
 finally:
     GPIO.cleanup()
