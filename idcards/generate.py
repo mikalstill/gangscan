9610aa6ae2f5ef93d442e7c41793f17f8348b88e
Michael Still <mikal@stillhq.com>
2019-06-07 08:37:59 +1000

    Changes to support gang show 2018.

--- idcards/generate.py
+++ idcards/generate.py
@@ -19,34 +19,17 @@ with open('db.csv') as f:
     for row in reader:
         pdf.add_page()
 
-        # Badge background colour
-        if row['Badge_Colour'] == 'Red':
-            pdf.set_fill_color(208, 92, 53)
-        elif row['Badge_Colour'] == 'Yellow':
-            pdf.set_fill_color(246, 196, 68)
-        elif row['Badge_Colour'] == 'Green':
-            pdf.set_fill_color(169, 181, 69)
-        else:
-            pdf.set_fill_color(255, 255, 255)
-        pdf.rect(0, 0, 55, 86, 'F')
-
         # Logo and text
         pdf.set_y(3)
         pdf.image('logo.jpg', w=49, x=3)
 
         pdf.set_y(55)
         pdf.multi_cell(w=0, h=6, border=0, align='C',
-                       txt='%s\n%s\n%s' %(row['Name'],
-                                          row['Team_Name'],
-                                          row['Patrol']))
+                       txt='%s\n%s' %(row['Name'], row['Team']))
 
         # Icons
-        if row['Age_Group'] == '18 and Over':
-            pdf.image('icon_over_18.png', w=8, x=10, y=75)
-        if row['Medical'] == 'Yes':
+        if row['Health'] == 'Yes':
             pdf.image('icon_medical.png', w=8, x=(55-8)/2, y=75)
-        if row['Dietary'] == 'Yes':
-            pdf.image('icon_allergy.png', w=8, x=(45-8), y=75)
 
         print('... %s' % row['Name'])
 
