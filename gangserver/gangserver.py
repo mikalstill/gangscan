84f0b2e2fc29138ed201d6beba94194a02431f7c
Michael Still <mikal@stillhq.com>
2019-07-08 19:04:34 +1000

    Create framework for manual events.

--- gangserver/gangserver.py
+++ gangserver/gangserver.py
@@ -8,7 +8,9 @@ import jinja2
 import json
 import mimetypes
 import os
+import requests
 import time
+import uuid
 
 import eventlog
 import filequeue
@@ -86,44 +88,9 @@ class Root(flask_restful.Resource):
 
 
 class Event(flask_restful.Resource):
-    def __init__(self):
-        self.queue = filequeue.FileQueue(
-            os.path.expanduser('~/gangserver-%s' % macaddress))
-
     def put(self, event_id):
         data = json.loads(flask.request.form['data'])
-        data['timestamp-server'] = time.time()
-
-        # Events look like this:
-        # {
-        #     "cardid": 744452027206, 
-        #     "device": "gangscan1", 
-        #     "event_id": "5362ef36-3f43-41d5-b7ac-e47b400a664f", 
-        #     "location": "out", 
-        #     "outcome": true, 
-        #     "owner": "Ben Ellis", 
-        #     "sha": "35ef6d", 
-        #     "signature": "55e1ffa7b1...cb54463f98f46b578", 
-        #     "timestamp-device": 1553634527.921784, 
-        #     "timestamp-server": 1553634528.421658, 
-        #     "timestamp-transferred": 1553634527.924872
-        # }
-        with open(os.path.expanduser('~/gangserver-status.json')) as f:
-            status = json.loads(f.read())
-        status[data['owner']] = data['location']
-        with open(os.path.expanduser('~/gangserver-status.json'), 'w') as f:
-            f.write(json.dumps(status, indent=4, sort_keys=True))
-
-        util.log('Stored event %s: %s' %(event_id, data))
-        self.queue.store_event('received', event_id, data)
-
-        event_log.add(event_id, data['cardid'], data['device'],
-                      data['location'], data['owner'],
-                      data['timestamp-device'], data['timestamp-server'])
-        queue.change_state('received', 'logged', event_id)
-        util.log('Logged event %s' % event_id)
-
-        return {event_id: data}
+        return process_event(data)
 
 
 class EventLog(flask_restful.Resource):
@@ -235,6 +202,87 @@ class Logout(flask_restful.Resource):
         return flask.redirect('/login')
 
 
+class ManualEvent(flask_restful.Resource):
+    def get(self):
+        # Enforce logins
+        if 'username' in flask.session:
+            username = flask.escape(flask.session['username'])
+        else:
+            return flask.redirect('/login')
+
+        # Read template and data
+        with open('gangserver/manualevent.html.tmpl') as f:
+            t = jinja2.Template(f.read())
+
+        # Do a dance to return HTML not JSON
+        resp = flask.Response(
+            t.render(),
+            mimetype='text/html')
+        resp.status_code = 200
+        return resp
+
+    def post(self):
+        # Enforce logins
+        if 'username' in flask.session:
+            username = flask.escape(flask.session['username'])
+        else:
+            return flask.redirect('/login')
+
+        data = {
+            'cardid': 'webui',
+            'device': str(username),
+            'event_id': str(uuid.uuid4()),
+            'location': flask.request.form['location'],
+            'outcome': True,
+            'owner': flask.request.form['person'],
+            'sha': None,
+            'timestamp-device': time.time()
+            }
+
+        h = hashlib.sha256()
+        for key in data:
+            s = '%s:%s' %(key, data[key])
+            h.update(s.encode('utf-8'))
+        data['signature'] = h.hexdigest()
+
+        process_event(data)
+        return flask.redirect('/')
+
+
+def process_event(data):
+    # Events look like this:
+    # {
+    #     "cardid": 744452027206, 
+    #     "device": "gangscan1", 
+    #     "event_id": "5362ef36-3f43-41d5-b7ac-e47b400a664f", 
+    #     "location": "out", 
+    #     "outcome": true, 
+    #     "owner": "Ben Ellis", 
+    #     "sha": "35ef6d", 
+    #     "signature": "55e1ffa7b1...cb54463f98f46b578", 
+    #     "timestamp-device": 1553634527.921784, 
+    #     "timestamp-server": 1553634528.421658
+    # }
+    data['timestamp-server'] = time.time()
+
+    with open(os.path.expanduser('~/gangserver-status.json')) as f:
+        status = json.loads(f.read())
+    status[data['owner']] = data['location']
+    with open(os.path.expanduser('~/gangserver-status.json'), 'w') as f:
+        f.write(json.dumps(status, indent=4, sort_keys=True))
+
+    util.log('Stored event %s: %s' %(data['event_id'], data))
+    queue.store_event('received', data['event_id'], data)
+
+    event_log.add(data['event_id'], data['cardid'], data['device'],
+                  data['location'], data['owner'],
+                  data['timestamp-device'], data['timestamp-server'])
+    queue.change_state('received', 'logged', data['event_id'])
+    util.log('Logged event %s' % data['event_id'])
+
+    return {data['event_id']: data}
+
+
 api.add_resource(Root, '/')
 api.add_resource(Event, '/event/<string:event_id>')
 api.add_resource(EventLog, '/eventlog')
@@ -242,6 +290,7 @@ api.add_resource(Health, '/health/<string:device>')
 api.add_resource(Local, '/local/<string:path>')
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+api.add_resource(ManualEvent, '/manualevent')
 
 # Process old received events
 queue = filequeue.FileQueue(os.path.expanduser('~/gangserver-%s'
